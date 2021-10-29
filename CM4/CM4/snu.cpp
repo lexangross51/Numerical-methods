41f5006247ac8d1b83be65045a139e25bdacc151
lexangross51 <56182229+lexangross51@users.noreply.github.com>
2021-10-29 14:48:31 +0700

    Программа полностью написана. Не работают. Процесс исправления ошибок...

--- CM4/CM4/snu.cpp
+++ CM4/CM4/snu.cpp
@@ -6,6 +6,7 @@ snu::snu(const std::string directory)
 {
 	n = m = maxIter = eps1 = eps2 = 0;
 	beta = 1;
+	currNormF = prevNormF = 0;
 	std::ifstream params(directory + "params.txt");
 
 	if (params.is_open())
@@ -26,6 +27,18 @@ snu::snu(const std::string directory)
 		for (size_t i = 0; i < n; i++)
 			initApprox >> x[i];
 		initApprox.close();
+	}
+}
+
+// Записать результат в файл
+void snu::writeToFile(const std::string file)
+{
+	std::ofstream out(file);
+	if (out.is_open())
+	{
+		for (size_t i = 0; i < x.size(); i++)
+			out << x[i] << std::endl;
+		out.close();
 	}
 }
 
@@ -120,20 +133,116 @@ void snu::createJacobiMatrix()
 			for (size_t j = 0; j < n; j++)
 				A[i][j] = df(i, j);
 	}
-	// Иначе нужно исключить m - n уравнений с минимальными
-	// |F|
+	// Иначе нужно исключить (m - n) уравнений с минимальными |F|
 	else
-	{
-		for (size_t l = 0, i = 0, k = 0; l < m; l++)
-		{
-			if (l != minF[k])
+		for (size_t l = 0, i = 0; l < m; l++)
+			if (!isInMinF(l))
 			{
 				for (size_t j = 0; j < n; j++)
 					A[i][j] = df(i, j);
 				F[i++] = F[l];
 			}
-			else
-				k = k + 1 < m - n ? k + 1 : k;
+}
+
+// Метод Гаусса
+void snu::gauss()
+{	
+	deltaX.resize(n);
+	real verySmall = 1e-10;
+
+	for (size_t i = 0; i < n; i++)
+	{
+		// Ищем максимальный элемент в столбце
+		size_t max = i;
+		for (size_t j = i + 1; j < n; j++)
+			if (fabs(A[j][i]) > fabs(A[max][i]))
+				max = j;
+
+		// Меняем текущую строку со строкой, в которой
+		// находится максимальный элемент
+		for (size_t j = i; j < n; j++)
+			std::swap(A[i][j], A[max][j]);
+		std::swap(F[i], F[max]);
+
+		// Вычитаем из всех строк, расположенных ниже
+		// текущей, строку с максимальным элементом,
+		// умноженную на вычисленный коэффициент.
+		// То же самое делаем для вектора
+		for (size_t j = i + 1; j < n; j++)
+		{
+			if (abs(A[i][i]) < verySmall)
+				throw snu::exceptions::VERY_SMALL_DIAG;
+
+			real m = A[j][i] / A[i][i];
+			F[j] -= m * F[i];
+			for (size_t k = i; k < n; k++)
+				A[j][k] -= m * A[i][k];
 		}
+
+		if (abs(A[i][i]) < verySmall)
+			throw snu::exceptions::VERY_SMALL_DIAG;
+
+		real m = A[i][i];
+		for (size_t j = i; j < n; j++)
+			A[i][j] /= m;
+		F[i] /= m;
 	}
+
+	// Обратный ход
+	for (int i = n - 1; i >= 0; i--)
+	{
+		real prod = F[i];
+		for (int j = n - 1; j >= i; j--)
+			prod -= A[i][j] * deltaX[j];
+
+		if (abs(A[i][i]) < verySmall)
+			throw snu::exceptions::VERY_SMALL_DIAG;
+
+		prod /= A[i][i];
+		deltaX[i] = prod;
+	}
+}
+
+// Ищем x(k+1)
+void snu::calc_xk1()
+{
+	beta = 1;
+	prevX = x;
+	for (size_t i = 0; i < x.size(); i++)
+		x[i] += deltaX[i];
+
+	f();
+	currNormF = norm(F);
+	for (size_t v = 0; beta > eps1 && currNormF > prevNormF; v++)
+	{
+		beta /= 2;
+		x = prevX + beta * deltaX;
+		f();
+		currNormF = norm(F);
+	}
+}
+
+// Метод Ньютона
+void snu::newton()
+{
+	if (n > n)
+		throw snu::exceptions::CANT_SOLVE;
+
+	f();
+
+	currNormF = norm(F);
+	prevNormF = currNormF;
+
+	for (size_t i = 0; i < maxIter && currNormF / prevNormF > eps2; i++)
+	{
+		if (m > n)
+			findMinF();
+		createJacobiMatrix();
+
+		gauss();
+
+		calc_xk1();
+	}
+
+	writeToFile("result.txt");
 }
--- CM4/CM4/snu.h
+++ CM4/CM4/snu.h
@@ -11,15 +11,24 @@ typedef std::vector<std::vector<real>> Matrix;
 
 class snu
 {
+public:
+	enum class exceptions { VERY_SMALL_DIAG, CANT_SOLVE };
 
 public:
 	snu(const std::string directory);
 
 	real f(size_t funcNumber);
-	void f();
 	real df(size_t funcNumber, size_t varNumber);
 
+	void f();
 	void createJacobiMatrix();
+
+	void gauss();
+	void newton();
+
+	void calc_xk1();
+
+	void writeToFile(const std::string file);
 
 private:
 	size_t m;		// Кол-во уравнений
@@ -29,7 +38,11 @@ private:
 	real eps2;		// Точность для невязки
 
 	Vector F;		// Вектор правой части
+	real currNormF;	// Текущая норма F
+	real prevNormF;	// Предыдущая норма F
 	Vector x;		// Вектор неизвестных
+	Vector prevX;	// Предыдущее значение x
+	Vector deltaX;	// Направление
 	real beta;		// Параметра бетта
 
 	Matrix A;		// Матрица Якоби
@@ -42,6 +55,22 @@ private:
 
 	bool isInMinF(size_t equationNum);
 };
+
+inline Vector operator +(const Vector& a, const Vector& b)
+{
+	Vector res = a;
+	for (size_t i = 0; i < res.size(); i++)
+		res[i] += b[i];
+	return res;
+}
+
+inline Vector operator *(const real& a, const Vector& b)
+{
+	Vector res = b;
+	for (size_t i = 0; i < res.size(); i++)
+		res[i] *= a;
+	return res;
+}
 
 #endif
 
